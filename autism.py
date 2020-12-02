1b52b64f9778162acd5c09fe665b445250ba1f3c
binwoes <xff@i2c-clk.org>
2020-12-02 17:45:30 -0500

    awful

--- autism.py
+++ autism.py
@@ -5,47 +5,18 @@ import re
 import exrex
 import sys
 
-width, height = 88, 88
-
 COLORS = [
-	# r" ",
-	# r"\.",
-	# r"[\'\`]",
-	# r"\"",
-	# r"\:",
-	# r";",
-	# r"\!",
-	# r"\|",
-	# r"[\?]",
-	# r"[\-\+\_]",
-	# r"[a-z0-9]",
-	# r"[A-Z]",
-	# r"#",
-	# r"\@"
 	r" ",
-	r"[\.\,]",
-	r"[\'\"]",
-	r"\:\;",
-	r"[a-z0-9]",
-	r"[A-Z]",
-	r"[\@\#]"
+	r"\.",
+	r"\'",
+	r"\:",
+	r"[a-z]",
+	r"[0-9]",
+	r"\@",
+	r"\#",
+	r"\|",
+	r"\!"
 ]
-
-code = open("Ch4Practice.java", "rb").read()
-
-image = PIL.Image.open("Untitled.png")
-image.load()
-image.thumbnail((width, height), PIL.Image.ANTIALIAS)
-image = PIL.ImageOps.grayscale(image)
-image = image.quantize(len(COLORS))
-
-width, height = image.size
-
-def ascii_to_color(char):
-	for color, pattern in enumerate(COLORS):
-		if re.match(pattern, char):
-			return color
-	return 2
 
 def color_to_pattern(color):
 	for _color, pattern in enumerate(COLORS):
@@ -97,58 +68,84 @@ def tokenize_java(code):
 	code = subn_but_careful(rb"\s+", b" ", code)
 	return re.findall(rb"[^\s]+", code)
 
-output = open("output.txt", "wb")
-
-tokens = tokenize_java(code)
-token_offset = 0
-
 def pixel_lookahead(image, x, y, distance):
 	buffer = []
-	for n in range(0,distance):
+	for n in range(0, distance):
 		buffer.append(image.getpixel((x+n, y)))
 	return buffer
 
-def color_distance(buffer1, buffer2):
-	total = 0
-	for n in range(0,len(buffer1)):
-		total += abs(buffer1[n] - buffer2[n])
-	return total
+def can_emit(buffer, length, max):
+	if [len(item[0]) for item in re.findall(r"((.)\2*)", "".join(map(str, buffer)))] == length:
+		pos = "".join(map(str, buffer)).find(item[0])
+		if pos + length < max - 4:
+			return True, pos
+	return False, 0
 
-image.save("gay.png")
+if __name__ == '__main__':
+	if len(sys.argv) != 4:
+		print(f"Usage: {sys.argv[0]} <source code> <image> <output>\n")
+		sys.exit(1)
+	input = open(sys.argv[1], "rb").read()
+	tokens = tokenize_java(input)
+	image = PIL.Image.open(sys.argv[2])
+	image.load()
+	image = image.resize((int(image.size[0] / 8), int(image.size[1] / 16)), PIL.Image.LANCZOS) # for some *cursed* reason thumbnail feels the compulsive urge to maintain aspect ratio
+	image = PIL.ImageOps.grayscale(image)
+	image = image.quantize(len(COLORS))
 
-for y in range(0, height-1):
-	skip_rows = 0
-	for x in range(0, width-1):
-		if skip_rows:
-			skip_rows -= 1
-			continue
+	width, height = image.size
 
-		token = None if token_offset+1 > len(tokens) else tokens[token_offset]
-		token_size = len(token) if token else 0
+	print(f"Output will be {width}x{height}")
 
-		distance_left = width - 1 - x 
+	output = open(sys.argv[3], "wb")
+	token_offset = 0
 
-		if tokens and token_size >= width-1: # if its bigger - then fuck it
-			output.write(token + b" ")
-			token_offset += 1
-			skip_rows += token_size + 1
+	for y in range(0, height):
+		distance_left = width
+		for x in range(0, width):
+			# ask nicely for a token
+			token = None if token_offset + 1 > len(tokens) else tokens[token_offset]
+			token_size = len(token.decode("utf8")) if token else 0
 
-		elif tokens and distance_left >= token_size and color_distance(pixel_lookahead(image, x, y, token_size), token) < token_size*150: # if code looks kinda like the image
-			output.write(token + b" ")
-			token_offset += 1
-			skip_rows += token_size + 1
+			#print(y, token, token_size, distance_left)
 
-		# elif distance_left > 20:
-		# 	output.write(b"/*")
-		# 	for pixel in pixel_lookahead(image, x, y, distance_left)[2:-2]:
-		# 		output.write(exrex.getone(color_to_pattern(pixel)).encode("utf8"))
-		# 	output.write(b"*/")
-		# 	skip_rows += distance_left
-			
-		else:
-			output.write(b" ")
-			# skip_rows += distance_left
+			if token and distance_left > token_size and len(set(pixel_lookahead(image, x, y, token_size))) == 1:
+				# if the string here is the same character repeated and it is the size of our token emit
+				output.write(token)
+				token_offset += 1
+				distance_left -= token_size
+			elif token and distance_left > token_size and can_emit(pixel_lookahead(image, x, y, distance_left), token_size, distance_left):
+				# if we find a needle, search for pos and write padding until we can write the requested token
+				# this is awful
+				_, pos = can_emit(pixel_lookahead(image, x, y, distance_left), token_size, distance_left)
+				output.write(b"/*")
+				distance_left -= 2
+				for n in range(0, pos):
+					output.write(exrex.getone(color_to_pattern(pixel_lookahead(image, x, y, distance_left)[1:-1][n])).encode("utf8"))
+				output.write(b"*/")
+				distance_left -= 2
+				output.write(token)
+				token_offset += 1
+				distance_left -= token_size
+			elif distance_left >= 4:
+				# add a comment block
+				output.write(b"/*")
+				distance_left -= 2
+				for pixel in pixel_lookahead(image, x, y, distance_left)[1:-1]:
+					output.write(exrex.getone(color_to_pattern(pixel)).encode("utf8"))
+					distance_left -= 1
+				output.write(b"*/")
+				distance_left -= 2
+				break
+			elif distance_left >= 2:
+				output.write(b"//" + b"/" if distance_left == 3 else b"")
+				distance_left -= distance_left
+				break
+			else:
+				# quite the travesty, we cannot do anything here
+				#print("Can't emit comment. tears in rain... Time to die") # <3 keybagd
+				break
+		output.write(b"\n")
 
-	output.write(b"\n")
-
-output.write(b" ".join(tokens[token_offset:]))
+	if tokens[token_offset:]:
+		output.write(b"/* extra tokens */\n" + b" ".join(tokens[token_offset:]))
